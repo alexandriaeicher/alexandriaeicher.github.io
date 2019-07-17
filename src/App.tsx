9e6a8155c9f8a189e3ca621b76665ad1c5f2a1b4
Alexandria Eicher <alexandriaeicher@gmail.com>
2019-07-16 21:55:51 -0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {Task} from "./models/task";
+import {NewTaskForm} from "./components/NewTaskForm";
+
+interface State {
+    newTask: Task;
+    tasks: Task[];
+}
+
+class App extends Component<{}, State> {
+    state = {
+        newTask: {
+            id: 1,
+            name: ""
+        },
+        tasks: []
+    };
+
+    private addTask = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        this.setState(previousState => ({
+            newTask: {
+                id: previousState.newTask.id + 1,
+                name: ""
+            },
+            tasks: [...previousState.tasks, previousState.newTask]
+        }));
+    };
+
+    private handleTaskChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            newTask: {
+                ...this.state.newTask,
+                name: event.target.value
+            }
+        });
+    };
+
+    private deleteTask = (taskToDelete: Task) => {
+        this.setState(previousState => ({
+            tasks: [
+                ...previousState.tasks.filter(task => task.id !== taskToDelete.id)
+            ]
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Hello React TS!</h2>
+                <NewTaskForm
+                    task={this.state.newTask}
+                    onAdd={this.addTask}
+                    onChange={this.handleTaskChange}
+                />
+            </div>
+        );
+    }
+}
+
+export default App;
